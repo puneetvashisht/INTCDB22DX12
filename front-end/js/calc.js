acab6d8aa0629d8baa40a12ee60987f516ef2def
Puneet Vashisht <puneetvashisht@outlook.com>
2022-03-02 11:08:44 +0530

    jquery & css

---
+++ front-end/js/calc.js
@@ -0,0 +1,14 @@
+function add(){
+    var oper1 = document.getElementById('oper1').value;
+    var oper2 = document.getElementById('oper2').value;
+    var result = parseInt(oper1) + parseInt(oper2);
+    console.log(result)
+    document.getElementById('result').innerText = 'Result is  ' + result
+}
+function sub(){
+    var oper1 = document.getElementById('oper1').value;
+    var oper2 = document.getElementById('oper2').value;
+    var result = parseInt(oper1) - parseInt(oper2);
+    console.log(result)
+    document.getElementById('result').innerText = 'Result is  ' + result
+}
