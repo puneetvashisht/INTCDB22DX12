43f89cc3c0181bdf73711848bcb511ecc5ddeaa1
Harshit Tomar <harshit.tomar@techknowheight.com>
2022-03-17 13:33:52 +0530

    crud

--- front-end/CLIENT/main.js
+++ front-end/CLIENT/main.js
@@ -35,3 +35,20 @@ function deleteCourse(id){
     .then(res => res.json())
     .then((res) => console.log(res))
 }
+
+
+
+function addData() {
+    let input1 = document.getElementById("in1").value
+    let input2 = document.getElementById("in2").value
+    fetch('http://localhost:8000/courses', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: input1,
+            summary: input2,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+}
